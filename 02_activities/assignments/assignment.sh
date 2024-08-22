5dfb7771e436d1e7ce62afc2f98842c120062b7d
ShrutiPatil-SAP <sp2911190@gmail.com>
2024-08-22 15:39:05 -0400

    Update assignment.sh
    completed the assignment.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -10,15 +10,28 @@ set -x
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
-
+ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+cd dir2
+touch file1.txt file2.txt file3.txt file4.txt file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3
+echo "Hello world" >> dir2/file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+cat dir2/file3.txt
 
 # 6. Delete file4
+# Delete the file named file4.txt in the dir2 directory
+rm dir2/file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
+rm -r dir2/dir4 dir2/dir5
+
+# Delete the directories dir4 and dir5 including all their contents
+rm -r dir4 dir5
+
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+cd /path/to/dir2
+ls
